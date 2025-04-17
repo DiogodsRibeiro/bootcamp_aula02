ba438aef252c444e1f39b6d02a798203544a160e
Diogo Souza <diogodsribeiro@gmail.com>
2025-04-16 23:10:14 -0300

    Exercicio 1

--- exercicios.py
+++ exercicios.py
@@ -3,6 +3,18 @@ import math
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+try: 
+    first_number = int(input('Digite um número:'))
+    second_number = int(input('Digite outro numero: '))
+
+    Resultado = first_number + second_number
+    print(f'O valor total é: {Resultado}')
+except ValueError:
+    print('Please, type a valid number!')cl
+
+
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
@@ -42,11 +54,12 @@ import math
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-data_usuario = input('insira uma data no formato dd/mm/aaaa: ')
-lista_dia_mes_ano = data_usuario.split("/")
-print(f'o elemento 1 é o: {lista_dia_mes_ano[0]}')
-print(f'o elemento 2 é o: {lista_dia_mes_ano[1]}')
-print(f'o elemento 3 é o: {lista_dia_mes_ano[2]}')
+# data_usuario = input('insira uma data no formato dd/mm/aaaa: ')
+# lista_dia_mes_ano = data_usuario.split("/")
+# print(f'o elemento 1 é o: {lista_dia_mes_ano[0]}')
+# print(f'o elemento 2 é o: {lista_dia_mes_ano[1]}')
+# print(f'o elemento 3 é o: {lista_dia_mes_ano[2]}')
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
